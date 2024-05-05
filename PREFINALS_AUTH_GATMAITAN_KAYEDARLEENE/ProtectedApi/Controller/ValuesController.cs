498d966499a1336a0769420c521cb266c2d97ef7
Gatmaitan_KayeDarleene_BSIT32E1 <161042740+Gatmaitan-KayeDarleene-BSIT32E1@users.noreply.github.com>
2024-05-06 01:13:41 +0800

    add code about myself

---
+++ PREFINALS_AUTH_GATMAITAN_KAYEDARLEENE/ProtectedApi/Controller/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ProtectedApi
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValueController : ControllerBase
+    {
+        private readonly string _owner = "Gatmaitan, Kaye Darleene";
+        private readonly Random _random = new Random();
+        private readonly string[] _thingsAboutOwner = new[]
+        {
+            "I play volleyball",
+            "I love watching kdrama",
+            "I like to eat ice cream",
+            "My favourite colour is pink",
+            "I hate cristian tagumpay",
+            "i love my family",
+            "i love my friends",
+            "I like to watch Haikyu",
+            "I'm 20 y/o",
+            "Currently taking BSIT",
+        };
+
+        [HttpGet("about/me")]
+        public IActionResult AboutMe()
+        {
+            var thing = _thingsAboutOwner[_random.Next(_thingsAboutOwner.Length)];
+            return Ok(thing);
+        }
+
+        [HttpGet("about")]
+        public IActionResult About()
+        {
+            return Ok(_owner);
+        }
+
+        [HttpPost("about")]
+        public IActionResult About([FromBody] NameModel model)
+        {
+            return Ok($"Hi {model.Name} from {_owner}");
+        }
+    }
+
+    public class NameModel
+    {
+        public string? Name { get; set; }
+    }
--- PREFINALS_AUTH_GATMAITAN_KAYEDARLEENE/ProtectedApi/obj/Debug/net8.0/ProtectedApi.AssemblyInfo.cs
+++ PREFINALS_AUTH_GATMAITAN_KAYEDARLEENE/ProtectedApi/obj/Debug/net8.0/ProtectedApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfbff2bfe9fb1148051f4be88783cfd92ded26cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df4fb70d38c1c5ca21e19726532a7f18f33abcad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
